5beb6616d3ec1c1ca2fdf6bc6b1af280b7847d75
bbungsang <821heejin@naver.com>
2017-06-14 01:37:46 +0900

    First Models

--- instargram/post/models.py
+++ instargram/post/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=36)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    photo = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    like_user = models.ManyToManyField(
+        User,
+        through='PostLike',
+    )
+
+
+class PostLike(models.Model):
+    user = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_ad = models.DateTimeField(auto_now=True)
+
+    like_user = models.ManyToManyField(
+        User,
+        through='CommentLike',
+    )
+
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(User)
+    comment = models.ForeignKey(Comment)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    pass
